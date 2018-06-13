5d3a9db8b7345a6ffc5901cf7add50e91eb7792f
Markus Maga <markus@nadilus.com>
2018-06-13 02:02:42 +0200

    Not rocking any crontab on arch

--- autoupdate/install.sh
+++ autoupdate/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # setups the auto-update
-(
-	crontab -l | grep -v "dot_update"
-	echo "0 */2 * * * $HOME/.dotfiles/bin/dot_update > ${TMPDIRR:-/tmp}/dot_update.log 2>&1"
-) | crontab -
+if which crontab >/dev/null 2>&1; then
+	(
+		crontab -l | grep -v "dot_update"
+		echo "0 */2 * * * $HOME/.dotfiles/bin/dot_update > ${TMPDIRR:-/tmp}/dot_update.log 2>&1"
+	) | crontab -
+else
+	echo "Err: Didn't find any crontab so can't setup autoupdate!"
+fi
