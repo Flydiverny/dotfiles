b7abb657f46d4ba855369f4b142c4bbe176e7e0b
Markus Maga <markus.maga@jayway.com>
2018-06-27 11:22:59 +0200

    Fix nvm check so nvm actually can get initialized if available..

--- nvm/autoload.zsh
+++ nvm/autoload.zsh
@@ -1,9 +1,9 @@
 #!/bin/zsh
-if which nvm >/dev/null 2>&1; then
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+if which nvm >/dev/null 2>&1; then
 	# # place this after nvm initialization!
 	autoload -U add-zsh-hook
 	load-nvmrc() {
