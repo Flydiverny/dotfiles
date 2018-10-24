8f4854c2b986571672d2658a3a706a346d98c2bd
Markus Maga <markus@nadilus.com>
2018-10-24 02:03:42 +0200

    Fix check for kubeprompt

--- kubernetes/prompt.zsh
+++ kubernetes/prompt.zsh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubeprompt="/usr/local/opt/kube-ps1/share/kube-ps1.sh"
-if [ -z "$kubeprompt" ]; then
+if [ -f "$kubeprompt" ]; then
   source $kubeprompt
-  prompt_newline=' $(kube_ps1)'$PREPROMPT$prompt_newline
+  prompt_newline=' $(kube_ps1)'${PREPROMPT}${prompt_newline}
 fi
