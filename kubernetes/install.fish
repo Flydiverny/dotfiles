e5e97abb251eb9bd4cbcdf4410a5b95a24c07c8e
Markus Maga <markus@maga.se>
2021-10-12 04:50:40 +0200

    fix: add krew to path
    Signed-off-by: Markus Maga <markus@maga.se>

--- kubernetes/install.fish
+++ kubernetes/install.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 curl -sL https://raw.githubusercontent.com/evanlucas/fish-kubectl-completions/master/completions/kubectl.fish -o ~/.config/fish/completions/kubectl.fish
 
+set -gx PATH $PATH $HOME/.krew/bin
+
 abbr -a kx kubectx
 abbr -a kn kubens
 
