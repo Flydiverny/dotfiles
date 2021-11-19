c4821ffbaadb40ef237b30dbef8437e5c7cd8d6a
Markus Maga <markus@maga.se>
2021-11-19 18:27:18 +0100

    fix: gcb to list all branches
    Signed-off-by: Markus Maga <markus@maga.se>

--- git/functions/gcb.fish
+++ git/functions/gcb.fish
@@ -1,7 +1,8 @@
 function gcb -d "Switch to git branch"
     switch $argv[1]
         case ''
-            set branch (git branch | fzf -d 10 | sed -e 's/\*//'  |sed -e 's/^[ \t]*//')
+set branch (git branch -a | fzf -d 10  | sed -e 's#remotes/origin/##' | sed -e 's/\*//'  |sed -e 's/^[ \t]*//')
+
             git switch $branch
         case '*'
             git switch $argv[1]
