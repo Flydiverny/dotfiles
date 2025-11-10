3013bf70f9b6a539d9c0cc15d12eb26bacf48e5f
Markus Maga <markus@maga.se>
2025-11-10 12:51:47 +0100

    fix: move env var
    Signed-off-by: Markus Maga <markus@maga.se>

--- fish/config.fish
+++ fish/config.fish
@@ -133,6 +133,8 @@ status is-interactive; and begin
 
         fish_add_path -a /opt/homebrew/bin || true
     end
+
+    set -x FZF_DEFAULT_OPTS "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
 end
 
 if test -f ~/.localrc.fish
