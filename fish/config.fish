cc3a29ee320aa316d8f250859c184d5c725847be
Markus Maga <markus@maga.se>
2025-11-10 13:40:35 +0100

    fix: eza icons flag
    Signed-off-by: Markus Maga <markus@maga.se>

--- fish/config.fish
+++ fish/config.fish
@@ -49,10 +49,10 @@ status is-interactive; and begin
     # ls
     if command -qs eza
         abbr --add ls eza
-        abbr --add l eza -lh --icons
-        abbr --add la eza -lah --icons
-        abbr --add ll eza -l --icons
-        abbr --add lt eza -l --icons --tree --level=2
+        abbr --add l eza -lh --icons=always
+        abbr --add la eza -lah --icons=always
+        abbr --add ll eza -l --icons=always
+        abbr --add lt eza -l --icons=always --tree --level=2
     else
         abbr --add l ls -lAh
         abbr --add la ls -A
